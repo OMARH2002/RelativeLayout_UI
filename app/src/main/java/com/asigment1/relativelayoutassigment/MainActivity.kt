86d2f1590a8ff70d1320d8292a9c7a47cff676e8
Omar Hany Mostafa <amorahany36002@gmail.com>
2024-08-05 21:37:43 +0300

    Initial commit

---
+++ app/src/main/java/com/asigment1/relativelayoutassigment/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.asigment1.relativelayoutassigment
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.asigment1.relativelayoutassigment.ui.theme.RelativeLayoutAssigmentTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+setContentView(R.layout.activity_main)
